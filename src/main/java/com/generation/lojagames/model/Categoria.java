a6af1d66f481f7f2f66b8d625c268cda6201aa53
Victoria_Moraes <victoria.candido.mm@gmail.com>
2023-12-14 13:22:08 -0300

    Model Produto e Categoria

--- src/main/java/com/generation/lojagames/model/Categoria.java
+++ src/main/java/com/generation/lojagames/model/Categoria.java
@@ -1,5 +1,42 @@
 package com.generation.lojagames.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+
 public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 64)
+	@NotBlank(message = "Declare o gênero do jogo cadastrado.")
+	@Size(min = 3, max = 64, message = "O gênero do jogo cadastrado deve conter de 3 a 64 caracteres")
+	private String genero;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
 
 }
--- src/main/java/com/generation/lojagames/model/Produto.java
+++ src/main/java/com/generation/lojagames/model/Produto.java
@@ -1,5 +1,111 @@
 package com.generation.lojagames.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+
 public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 64)
+	@Size(min = 10, max = 64, message = "O nome do jogo precisa ter de 10 a 64 caracteres.")
+	@NotBlank(message = " O nome do jogo é obrigatório.")
+	private String nome;
+
+	@Column(length = 255)
+	@Size(min = 25, max = 255, message = "A descrição precisa ter de 10 a 255 caracteres.")
+	@NotBlank(message = " A descrição do jogo é obrigatória.")
+	private String descricao;
+
+	@Column(length = 50)
+	@Size(min = 2, max = 50, message = "O atributo Console precisa ter de 2 a 50 caracteres.")
+	@NotBlank(message = "É obrigatório especificar o console no qual o jogo funciona.")
+	private String console;
+
+	@Column(length = 500)
+	@Size(min = 25, max = 500, message = "O Atributo imagem precisa ter de 25 a 500 caracteres")
+	@NotBlank(message = "É necessário a inclusão da imagem ilustrativa do jogo!")
+	private String imagem;
+
+	@DecimalMin(value = "0.0", inclusive = false)
+	@Digits(integer = 3, fraction = 2)
+	private BigDecimal preco;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dataLancamento;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
 
 }
